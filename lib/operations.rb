1f98a5f4d95c7c498910b17ce702033b9a6abf03
Nakai <nakai.hamlin18@gmail.com>
2021-02-23 01:09:09 -0500

    commit tree aa230eec63877603d3e8704870d1db85e61a3a0b

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+    if speed < 40 || speed > 60
+        return true
+    else false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? true : false
 end
 	
 
